472921a8d63e8e81aa897902dfb8188692be5c1a
xMintTea <minttea77105110116@gmail.com>
2025-09-13 19:01:06 +0800

    refactor: перенёс get_db в отдельный модуль Смотри db_helpher.py

--- main.py
+++ main.py
@@ -61,12 +61,7 @@ Base.metadata.create_all(bind=engine)
 
 
 
-def get_db():
-    db = session_local()
-    try:
-        yield db
-    finally:
-        db.close()
+
 
 
 def get_hash(string: str) -> str:
