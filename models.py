be71eb1e32b122aa670d7794478dcceb06f598f8
xMintTea <minttea77105110116@gmail.com>
2025-10-31 14:53:26 +0800

    feat: rewrited models

--- models.py
+++ models.py
@@ -2,126 +2,170 @@ from sqlalchemy import (
     Column,
     Integer,
     String,
-    DateTime,
-    Enum,
     ForeignKey,
-    Text,
-    func
+    Enum
 )
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
 from database import Base
 from static import Roles
 
 
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nickname = Column(String)
-    login = Column(String, unique=True, index=True)
-    password = Column(String)
-    email = Column(String, unique=True, index=True)
-    role = Column(Enum(Roles), default=Roles.USER)
-    title_id = Column(Integer, ForeignKey("titles.id"), nullable=True)
+class BaseModel(Base):
+    __abstract__ = True
+    __allow_unmapped__ = True
     
-    title = relationship("Title", back_populates="users")
-    student = relationship("Student", back_populates="user", uselist=False)
-    achievements = relationship("Achievement", secondary="user_achievements", back_populates="users")
-    profile = relationship("UserProfile", back_populates="user", uselist=False)
+    id = Column(Integer, primary_key=True)
 
-class Student(Base):
-    __tablename__ = "students"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'), unique=True)    
+
+
+
+class StudentCourse(BaseModel):
+    __tablename__  = "student_course"
+    student_id = Column("student_id", Integer, ForeignKey("users.id"))
+    course_id = Column("course_id", Integer, ForeignKey("courses.id"))
+
+class StudentTopics(BaseModel):
+    __tablename__ = "student_topic"
+    student_id = Column("student_id", Integer, ForeignKey("users.id"))
+    topic_id = Column("topic_id", Integer, ForeignKey("topics.id"))
+
+class UserProfile(BaseModel):
+    __tablename__ = "users_profiles"
+    
+    user_id = Column(ForeignKey("users.id"))
+    user = relationship("User", back_populates="profile")
+    
+    about_me = Column(String)
+    
+
+    current_frame_id = Column(Integer)
+    available_frames = relationship("Frame")
+    
+    current_title = Column(Integer)
+    available_titles = relationship("Title")
+    
+    achievements = relationship("Achievement")
+
+    def __repr__(self):
+        return f"<UserProfile({self.id=},{self.current_frame_id=}, {self.available_frames=})>"
+
+
+class GamificationRecord(BaseModel):
+    __tablename__ = "game_records"
+    
+    user_id = Column(ForeignKey("users.id"))
+    user = relationship("User", back_populates="gamificationRecord")
+    
     xp = Column(Integer, default=0)
     lvl = Column(Integer, default=1)
     currency = Column(Integer, default=0)
-    current_course_id = Column(Integer, ForeignKey('courses.id')) 
     
-    user = relationship("User", back_populates="student")
-    current_course = relationship("Course", back_populates="students") 
+    def __repr__(self):
+        return f"<GameRecord({self.id=}, {self.user_id=}, {self.xp=})>"
 
-class Achievement(Base):
+
+class User(BaseModel):
+    __tablename__ = "users"
+    
+    login = Column(String)
+    password = Column(String)
+    role = Column(Enum(Roles))
+
+    profile = relationship(UserProfile,back_populates="user", uselist=False) # 1 к 1
+    gamificationRecord = relationship(GamificationRecord, back_populates="user", uselist=False) # 1 к 1
+    courses = relationship("Course", secondary="student_course", back_populates="users")
+
+    completed_topics = relationship("Topic", secondary="student_topic", back_populates="users_completed")
+
+    def __repr__(self):
+        return f"<User({self.id=}, {self.login=}, {self.profile=})>"
+
+
+class Frame(BaseModel):
+    __tablename__ = "frames"
+    
+    name = Column(String)
+    img_href = Column(String)
+    
+
+    user_id = Column(ForeignKey("users_profiles.id"))
+    uploader_id = Column(Integer)
+
+    def __repr__(self):
+        return f"<Frame({self.id=}, {self.name=})>"
+
+
+class Achievement(BaseModel):
     __tablename__ = "achievements"
-    id = Column(Integer, primary_key=True, index=True)
+    
+    user_id = Column(ForeignKey("users_profiles.id"))
+    
     name = Column(String)
     description = Column(String)
-    
-    users = relationship("User", secondary="user_achievements", back_populates="achievements")
 
-class UserAchievement(Base):
-    __tablename__ = "user_achievements"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    achievement_id = Column(Integer, ForeignKey('achievements.id'))
-    
-    user = relationship("User")
-    achievement = relationship("Achievement")
 
-class Title(Base):
+class Title(BaseModel):
     __tablename__ = "titles"
-    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(ForeignKey("users_profiles.id"))
+    
     name = Column(String)
-    
-    users = relationship("User", back_populates="title")
 
-class UserProfile(Base):
-    __tablename__ = "user_profiles"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), unique=True)
-    about_me = Column(String, nullable=True)
-    
-    user = relationship("User", back_populates="profile")
 
-class Course(Base):
+class Course(BaseModel):
     __tablename__ = "courses"
     
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    description = Column(Text)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-    
-    topics = relationship("Topic", back_populates="course", order_by="Topic.order")
-    students = relationship("Student", back_populates="current_course") 
+    name = Column(String)
+    description = Column(String)
+    users = relationship(User, secondary="student_course", back_populates="courses")
 
-class Topic(Base):
+    topics = relationship("Topic")
+
+
+    def __repr__(self):
+        return f"<Course({self.name=},{self.topics=})>"
+
+class Topic(BaseModel):
     __tablename__ = "topics"
     
-    id = Column(Integer, primary_key=True, index=True)
-    course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
-    title = Column(String, nullable=False)
-    content = Column(Text)
-    order = Column(Integer, default=0)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    course_id = Column(ForeignKey("courses.id"))
     
-    course = relationship("Course", back_populates="topics")
+    
+    name = Column(String)
+    content = Column(String)
+    order = Column(Integer)
+    
+    users_completed = relationship(User, secondary="student_topic", back_populates="completed_topics")
 
 
-class TestQuestion(Base):
+    def __repr__(self):
+        return f"<Topic({self.name=},{self.users_completed=})>"
+
+
+class TestQuestion(BaseModel):
     __tablename__ = "test_questions"
     
-    id = Column(Integer, primary_key=True, index=True)
-    text = Column(Text, nullable=False)  # Текст вопроса
-    order = Column(Integer, default=0)   # Порядок вопроса (для сортировки)
+    text = Column(String)
+    order = Column(Integer)
+    
+    answers = relationship("TestAnswer")
 
-class TestAnswerOption(Base):
-    __tablename__ = "test_answer_options"
+    def __repr__(self):
+        return f"<TestQuestion({self.id=}, {self.answers=})>"
+
+
+class TestAnswer(BaseModel):
+    __tablename__ = "test_answers"
     
-    id = Column(Integer, primary_key=True, index=True)
-    question_id = Column(Integer, ForeignKey("test_questions.id"))
-    answer_text = Column(String, nullable=False)  # Текст ответа
-    creative_value = Column(Integer, default=0)   # Баллы за творчество
-    analytical_value = Column(Integer, default=0) # Баллы за аналитику
-    
-    question = relationship("TestQuestion", backref="answer_options")
-    
-    
-class SocialConnection(Base):
-    __tablename__ = "users_social_connections"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    
+    question_id = Column(ForeignKey("test_questions.id"))
+    text = Column(String)
+    analytical_value = Column(Integer)
+    creative_value = Column(Integer)
+
+
+    def __repr__(self):
+        return f"<TestAnswer({self.id=})>"
+
+
