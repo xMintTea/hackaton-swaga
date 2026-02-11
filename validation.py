993e6ec0ae61f70b3837fc30c7e3f32e692f4a7b
xMintTea <minttea77105110116@gmail.com>
2026-02-11 18:24:55 +0800

    feat: change user's content

--- validation.py
+++ validation.py
@@ -2,13 +2,14 @@ from utils.helpers import TOKEN_TYPE_FIELD, ACCESS_TOKEN_TYPE, REFRESH_TOKEN_TYP
 from fastapi import HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer
 from jwt import InvalidTokenError
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, Query
 
 
 from auth import utils_jwt
 from schemas.users import User as UserSchema
 from models import User
 from utils.db_helpher import get_db
+from routers.users import get_users
 
 oath2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login/",)
 
@@ -40,10 +41,11 @@ def get_current_token_payload(
     return payload
     
 
-def get_user_by_token_sub(payload: dict, db: Session) -> UserSchema:
+def get_user_by_token_sub(payload: dict, users: Query) -> UserSchema:
     user_id: str | None = payload.get("sub")
 
-    if user:=db.query(User).where(User.id == user_id).first():
+    if user:=users.where(User.id == user_id).first():
+        
         return user
     
 
@@ -54,10 +56,10 @@ def get_user_by_token_sub(payload: dict, db: Session) -> UserSchema:
 def get_auth_user_from_token_of_type(token_type: str):
     def get_auth_user_from_token(
         payload: dict = Depends(get_current_token_payload),
-        db: Session = Depends(get_db)
+        users: Query = Depends(get_users)
     ) -> UserSchema:
         validate_token_type(payload,token_type)
-        return get_user_by_token_sub(payload, db)
+        return get_user_by_token_sub(payload, users)
     
     return get_auth_user_from_token
 
